a0c7d558d1c90740bf736af992cdfa1b0819b733
Student <student@C17775454-lnx.offcampusnetwork.co.uk>
2018-02-19 17:16:52 +0000

    Added information about event queue and call backs.

--- src/main.cpp
+++ src/main.cpp
@@ -12,17 +12,19 @@ void blink(void){
 }
 
 Serial pc(USBTX, USBRX);
+// This thread will run the event queues dispatch funciton
 Thread worker;
-EventQueue queue ;
+EventQueue queue ; // Creates a queue to hold events
 
 Thread thread;
 
 int main() {
     pc.printf("ready");
-    queue.call_every(500, flash);
+    queue.call_every(500, flash); // Add to the event queue
     queue.call_every(300, blink);
     pc.printf("dispatching...");
     queue.dispatch_forever();
+    // This starts the event queues dispatch thread.
     //worker.start(callback(&queue, &EventQueue::dispatch_forever ));
     pc.printf("dispatched");
     while(1){}
